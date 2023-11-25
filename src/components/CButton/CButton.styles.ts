fb99e8a8283263b5593bcff18e2c7a21aa68a978
Jose Cordova <jmcordova1999@gmail.com>
2023-11-24 22:29:02 -0500

    feat: font inherit to button

--- src/components/CButton/CButton.styles.ts
+++ src/components/CButton/CButton.styles.ts
@@ -1,6 +1,7 @@
 import styles from 'styled-components';
 
 export const CButtonStyled = styles.button`
+	font-family: inherit;
 	background-color: hsl(278, 68%, 11%);
 	color: hsl(0, 0%, 100%);
 	padding: 0.95rem 0;
